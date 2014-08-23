167d320e4b7300b333fb892fec5675fb7392b951
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2014-08-24 00:04:25 +0200

    added a Cat model ready for CRUD

--- Handler/Cats.hs
+++ Handler/Cats.hs
@@ -7,3 +7,15 @@ getCatsR = do
   defaultLayout $ do
     setTitle "Some Cats"
     $(widgetFile "cats")
+
+putCatsR :: Handler Html
+putCatsR = error "putCatsR not yet defined"
+
+getCatR :: Text -> Handler Html
+getCatR _ = error "getCatR not yet defined"
+
+putCatR :: Text -> Handler Html
+putCatR _ = error "getCatR not yet defined"
+
+deleteCatR :: Text -> Handler Html
+deleteCatR _ = error "getCatR not yet defined"
