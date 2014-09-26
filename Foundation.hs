a503ef49cff30e320043b4166b7200de328cbd92
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2014-09-26 23:46:36 +0200

    added a login/logout navbar

--- Foundation.hs
+++ Foundation.hs
@@ -65,6 +65,7 @@ instance Yesod App where
     defaultLayout widget = do
         master <- getYesod
         mmsg <- getMessage
+        muser <- maybeAuthId
 
         -- We break up the default layout into two components:
         -- default-layout is the contents of the body tag, and
