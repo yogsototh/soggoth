c77b0d114f9fcd4d3005b59418a9e3b2c940c747
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2014-10-03 18:51:08 +0200

    externalized the template

--- Handler/Cats.hs
+++ Handler/Cats.hs
@@ -4,18 +4,4 @@ import Import
 
 getCatsR :: Handler Html
 getCatsR = do
-  defaultLayout $ [whamlet|
-    <div .hero-unit>
-      <h1>Some Cats
-    <div .container>
-      <div .row>
-        <div .span4>
-          <h2>Persian
-          <p>The Persian cat
-        <div .span4>
-          <h2>Savannah
-          <p>The Savannah cat
-        <div .span4>
-          <h2>Siamese
-          <p>The Siamese cat
-        |]
+  defaultLayout $(widgetFile "cats")
